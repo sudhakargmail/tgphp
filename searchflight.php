4bca9efc7d94ba4fc505f901c62c6769e2d7dc18
Sudhakar <sudhakar98@gmail.com>
2016-09-30 14:52:06 +0530

    searchflight php

--- searchflight.php
+++ searchflight.php
@@ -1,5 +1,5 @@
 <?php
-// parameters
+
 $hubVerifyToken = 'mymessengertoken';
 $accessToken = "EAAZAmqcuBqGoBAK13SY5H4KU7DnceRuBeOB8ymQqv5pufsqkTRl4gyXu2VVb7HfqbuWpA0lXtUio5sZARHS5RRX8d2Yf6G6UZBckHZBrZAiWZC7ZCzUnyrsRoJ6PzFnxIAwZCtku2fA9Y9aIZBDZBRM6ZA251lAuTvss9aLFsVL1hZCSdQZDZD";
 
@@ -13,4 +13,4 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 echo "{'messagingProvider': 'FACEBOOK', 'chatKey': '3648b2a2-1aee-4d5a-7ef4-13a0aa441cb1', 'attributes': {'redeye': true,                'twoWay': true}, 'departDateMax': '2016-05-31T00:00:00', 'departDateMin': '2016-05-31T00:00:00', 'returnDateMax': '2016-06-03T00:00:00', 'returnDateMin': '2016-06-03T00:00:00', 'origin': {'airports': ['LHR', 'LGW', 'LCY', 'STN'],            'allAirportsCode': 'LON',            'geoid': 2643743,            'latitude': 51.50853,            'longitude': -0.12574,            'name': 'London, United Kingdom',            'type': 'City'}, 'destination': {'airports': ['SVO', 'DME', 'VKO', 'BKA'],                 'allAirportsCode': 'MOW',                 'geoid': 524901,                 'latitude': 55.75222,                 'longitude': 37.61556,                 'name': 'Moscow, ussia',                 'type': 'City'}, 'travelers': {'adult': 3}}";
 
 
-/>
+?>

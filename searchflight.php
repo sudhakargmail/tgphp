d0fe4fd2af03425b0c4f0cedbc9aa91a8100b2b6
Sudhakar <sudhakar98@gmail.com>
2016-09-30 14:42:25 +0530

    searchflight php

--- searchflight.php
+++ searchflight.php
@@ -10,30 +10,7 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
 }
 
 
-String response = '{"messagingProvider": "FACEBOOK",
- "chatKey": "3648b2a2-1aee-4d5a-7ef4-13a0aa441cb1",
- "attributes": {"redeye": true,
-                "twoWay": true},
- "departDateMax": "2016-05-31T00:00:00",
- "departDateMin": "2016-05-31T00:00:00",
- "returnDateMax": "2016-06-03T00:00:00",
- "returnDateMin": "2016-06-03T00:00:00",
- "origin": {"airports": ["LHR", "LGW", "LCY", "STN"],
-            "allAirportsCode": "LON",
-            "geoid": 2643743,
-            "latitude": 51.50853,
-            "longitude": -0.12574,
-            "name": "London, United Kingdom",
-            "type": "City"},
- "destination": {"airports": ["SVO", "DME", "VKO", "BKA"],
-                 "allAirportsCode": "MOW",
-                 "geoid": 524901,
-                 "latitude": 55.75222,
-                 "longitude": 37.61556,
-                 "name": "Moscow, Russia",
-                 "type": "City"},
- "travelers": {"adult": 3}}';
-
+String response = "{'messagingProvider': 'FACEBOOK', 'chatKey': '3648b2a2-1aee-4d5a-7ef4-13a0aa441cb1', 'attributes': {'redeye': true,                'twoWay': true}, 'departDateMax': '2016-05-31T00:00:00', 'departDateMin': '2016-05-31T00:00:00', 'returnDateMax': '2016-06-03T00:00:00', 'returnDateMin': '2016-06-03T00:00:00', 'origin': {'airports': ['LHR', 'LGW', 'LCY', 'STN'],            'allAirportsCode': 'LON',            'geoid': 2643743,            'latitude': 51.50853,            'longitude': -0.12574,            'name': 'London, United Kingdom',            'type': 'City'}, 'destination': {'airports': ['SVO', 'DME', 'VKO', 'BKA'],                 'allAirportsCode': 'MOW',                 'geoid': 524901,                 'latitude': 55.75222,                 'longitude': 37.61556,                 'name': 'Moscow, ussia',                 'type': 'City'}, 'travelers': {'adult': 3}}";
 
 
 echo response;
